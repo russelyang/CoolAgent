860c40043643a8447662d59a64a1a95a62d65c97
xiaodong yang <ryang@xiaodong-yangs-iMac.local>
2010-11-10 22:21:20 -0800

    initial check in.

---
+++ Classes/CoolAgentAppDelegate.h
@@ -0,0 +1,34 @@
+//
+//  CoolAgentAppDelegate.h
+//  CoolAgent
+//
+//  Created by xiaodong yang on 10-11-09.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface CoolAgentAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
